de7ccc856106bb47f97e46f17f9bee5889a54544
yuichi <yuichi0x@gmail.com>
2014-01-04 23:55:08 +0900

    first commit.

---
+++ Win32Project1/Win32Project1/Resource.h
@@ -0,0 +1,13 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_ABOUTBOX                            103
+#define IDM_ABOUT                               104
+#define IDM_EXIT                                105
+#define IDI_WIN32PROJECT1                       107
+#define IDI_SMALL                               108
+#define IDC_WIN32PROJECT1                       109
+#define IDD_DIALOG1                             110
+#define IDM________1                            40000
+#define IDS_APP_TITLE                           40000
